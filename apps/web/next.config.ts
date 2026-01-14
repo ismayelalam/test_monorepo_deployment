233621d05797dfc0b83be54854d266a66f70af3a
Ismayel Alam <ismayelalam0@gmail.com>
2026-01-14 18:59:20 +0100

    test auto deploy

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 const API_URL = process.env.Backend_URL;
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
